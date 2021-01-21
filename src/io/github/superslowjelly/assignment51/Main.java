6d2ccab64718edfa30eadb2b3ed4c76c01c6a14a
Chae Taylor <chaetaylor@msn.com>
2021-01-21 21:10:56 +0000

    Remove unnecessary stringbuilder

--- src/io/github/superslowjelly/assignment51/Main.java
+++ src/io/github/superslowjelly/assignment51/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(10.5, 5.5);
-        StringBuilder output = new StringBuilder();
-        output.append("Rectangle:")
-                .append("\n\tWidth - ").append(rectangle.getWidth()).append("cm.")
-                .append("\n\tHeight - ").append(rectangle.getHeight()).append("cm.")
-                .append("\n\tPerimeter - ").append(rectangle.getPerimeter()).append("cm.")
-                .append("\n\tArea - ").append(rectangle.getArea()).append("cm².");
+        String output = "Rectangle:" +
+                "\n\tWidth - " + rectangle.getWidth() + "cm." +
+                "\n\tHeight - " + rectangle.getHeight() + "cm." +
+                "\n\tPerimeter - " + rectangle.getPerimeter() + "cm." +
+                "\n\tArea - " + rectangle.getArea() + "cm².";
         System.out.println(output);
     }
 
